b54f710ad02f4de75aa78139d22440834b42db40
shin <proscus0706@gmail.com>
2019-12-27 18:11:12 +0900

    entity

--- Concept/ContactManager/app/src/main/java/com/anushka/androidtutz/contactmanager/db/entity/Contact.java
+++ Concept/ContactManager/app/src/main/java/com/anushka/androidtutz/contactmanager/db/entity/Contact.java
@@ -1,25 +1,36 @@
 package com.anushka.androidtutz.contactmanager.db.entity;
 
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
 
+@Entity(tableName = "contacts")
 public class Contact {
 
-    public static final String TABLE_NAME = "contacts";
+//    public static final String TABLE_NAME = "contacts";
 
-    public static final String COLUMN_ID = "contact_id";
-    public static final String COLUMN_NAME = "contact_name";
-    public static final String COLUMN_EMAIL = "contact_email";
+//    public static final String COLUMN_ID = "contact_id";
+//    public static final String COLUMN_NAME = "contact_name";
+//    public static final String COLUMN_EMAIL = "contact_email";
 
+    @ColumnInfo(name = "contact_id")
     private String name;
+
+    @ColumnInfo(name = "contact_name")
     private String email;
+
+    @ColumnInfo(name = "contact_email")
+    @PrimaryKey(autoGenerate = true)
     private long id;
 
 
+    @Ignore
     public Contact() {
     }
 
-    public Contact(long id, String name, String email) {
 
+    public Contact(long id, String name, String email) {
         this.name = name;
         this.email = email;
         this.id = id;
@@ -52,10 +63,10 @@ public class Contact {
 
 
     // Create table SQL query
-    public static final String CREATE_TABLE =
-            "CREATE TABLE " + TABLE_NAME + "("
-                    + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
-                    + COLUMN_NAME + " TEXT,"
-                    + COLUMN_EMAIL + " DATETIME DEFAULT CURRENT_TIMESTAMP"
-                    + ")";
+//    public static final String CREATE_TABLE =
+//            "CREATE TABLE " + TABLE_NAME + "("
+//                    + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+//                    + COLUMN_NAME + " TEXT,"
+//                    + COLUMN_EMAIL + " DATETIME DEFAULT CURRENT_TIMESTAMP"
+//                    + ")";
 }
