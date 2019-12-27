98892e200274a56b5a6f8e1d4f1f2424bf5d72e8
shin <proscus0706@gmail.com>
2019-12-27 18:17:37 +0900

    ContactDAO

---
+++ Concept/ContactManager/app/src/main/java/com/anushka/androidtutz/contactmanager/db/ContactDAO.java
@@ -0,0 +1,31 @@
+package com.anushka.androidtutz.contactmanager.db;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.anushka.androidtutz.contactmanager.db.entity.Contact;
+
+import java.util.List;
+
+@Dao
+public interface ContactDAO {
+
+    @Insert
+    long addContact(Contact contact);
+
+    @Update
+    void updateContact(Contact contact);
+
+    @Delete
+    void deleteContact(Contact contact);
+
+    @Query("SELECT * FROM contacts")
+    List<Contact> getContacts();
+
+    @Query("SELECT * FROM contacts WHERE contact_id = :contactId")
+    Contact getContact(long contactId);
+
+}
